4830dce3d1d64ba82aae9a0ec344c55e904087f2
Sameer Aftab <sameeraftab64@gmail.com>
2025-07-25 19:09:56 +0000

    Still i get two scroller in Add studen'ts part

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -6,9 +6,9 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex h-screen bg-background">
+    <div className="flex bg-background">
       <AppSidebar />
-      <main className="flex-1 flex flex-col overflow-y-auto">
+      <main className="flex-1 flex flex-col min-h-screen">
           {children}
       </main>
     </div>
