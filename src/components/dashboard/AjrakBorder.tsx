b67acd2d0275ca25ac55a5c3d52a6d3359fe38b8
Sameer Aftab <sameeraftab64@gmail.com>
2025-07-26 12:04:15 +0000

    update border with this code import * as React from 'react';
    export fun

--- src/components/dashboard/AjrakBorder.tsx
+++ src/components/dashboard/AjrakBorder.tsx
@@ -2,31 +2,35 @@ import * as React from 'react';
 
 export function AjrakBorder() {
   return (
-    <svg width="100%" height="100%" style={{ position: 'absolute', top: 0, left: 0, zIndex: 1 }} xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
-        <defs>
-            <pattern id="border-pattern" patternUnits="userSpaceOnUse" width="10" height="10">
-                <g fill="hsl(var(--primary-foreground))">
-                    <rect width="10" height="10" fill="hsl(var(--primary))"/>
-                    <path d="M0 0 L5 5 L0 10 Z" />
-                    <path d="M10 0 L5 5 L10 10 Z" />
-                </g>
-            </pattern>
-            <pattern id="frame-pattern" patternUnits="userSpaceOnUse" width="100%" height="100%">
-              <rect width="100%" height="100%" fill="hsl(var(--foreground))"/>
-              <rect width="100%" height="30" y="35" fill="url(#border-pattern)"/>
-              <rect width="100%" height="30" y="75" fill="url(#border-pattern)"/>
-            </pattern>
-        </defs>
-        
-        {/* Top Border */}
-        <rect x="0" y="0" width="100%" height="30" fill="url(#frame-pattern)" />
+    <svg
+      viewBox="0 0 100 100"
+      preserveAspectRatio="none"
+      style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 1 }}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <defs>
+        {/* Ethnic-style triangle pattern */}
+        <pattern id="border-pattern" patternUnits="userSpaceOnUse" width="5" height="5">
+          <rect width="5" height="5" fill="white" />
+          <path d="M0,0 L2.5,2.5 L0,5 Z" fill="black" />
+          <path d="M5,0 L2.5,2.5 L5,5 Z" fill="black" />
+        </pattern>
+      </defs>
 
-        {/* Main Content Area Border */}
-        <rect x="0" y="30" width="30" height="calc(100% - 60px)" fill="url(#frame-pattern)" />
-        <rect x="calc(100% - 30px)" y="30" width="30" height="calc(100% - 60px)" fill="url(#frame-pattern)" />
+      {/* Red background */}
+      <rect width="100" height="100" fill="red" />
 
-        {/* Bottom Border */}
-        <rect x="0" y="calc(100% - 30px)" width="100%" height="30" fill="url(#frame-pattern)" />
+      {/* Top border */}
+      <rect x="0" y="0" width="100" height="5" fill="url(#border-pattern)" />
+
+      {/* Bottom border */}
+      <rect x="0" y="95" width="100" height="5" fill="url(#border-pattern)" />
+
+      {/* Left border */}
+      <rect x="0" y="5" width="5" height="90" fill="url(#border-pattern)" />
+
+      {/* Right border */}
+      <rect x="95" y="5" width="5" height="90" fill="url(#border-pattern)" />
     </svg>
   );
 }
